0dc58b5cb7c009c590f8502e2b740b3743fa504f
laurenfazah <laurenfazah@gmail.com>
2016-08-09 14:35:01 -0400

    Updated books migration for one-to-many starter code

--- db/migrate/20160807201015_create_books.rb
+++ db/migrate/20160807201015_create_books.rb
@@ -1,8 +1,8 @@
+# Original Books migration
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author
 
       t.timestamps null: false
     end
