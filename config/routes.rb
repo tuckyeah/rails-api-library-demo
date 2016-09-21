18626a085795e0484b997f76ff2cfb2ab214d8ca
tuckyeah <h.rosebrock@gmail.com>
2016-09-21 13:07:41 -0400

    complete CRUD functionality for books -- include models, controllers, and serializer

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  get '/books', to: 'books#index'
+  get '/books/:id', to: 'books#show'
+  post '/books', to: 'books#create'
+  patch '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
 end
